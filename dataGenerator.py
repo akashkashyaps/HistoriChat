e3393214102e098e849388e273c91d1d97cc495f
Akash <akashkashyaps123@gmail.com>
2025-04-07 15:28:39 +0100

    added langchain wrappers

--- dataGenerator.py
+++ dataGenerator.py
@@ -3,6 +3,7 @@ from langchain_community.chat_models import ChatOllama
 from langchain_ollama import OllamaEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from ragas.llms import LangchainLLMWrapper
+from ragas.embeddings import LangchainEmbeddingsWrapper
 from ragas.testset import TestsetGenerator
 
 
@@ -13,7 +14,9 @@ warnings.filterwarnings("ignore", category=LangChainDeprecationWarning)
 
 # Initialize Ollama components
 ollama_llm = ChatOllama(model="qwen2.5:7b-instruct-q4_0", temperature=0.2)
+llm = LangchainLLMWrapper(ollama_llm)
 ollama_embeddings = OllamaEmbeddings(model="nomic-embed-text") 
+embeddings = LangchainEmbeddingsWrapper(ollama_embeddings)
 
 
 # Load documents
@@ -29,7 +32,7 @@ text_splitter = RecursiveCharacterTextSplitter(
 split_docs = text_splitter.split_documents(docs)
 
 # Initialize test generator with Ollama components
-generator = TestsetGenerator.from_langchain(ollama_llm, ollama_llm, ollama_embeddings)
+generator = TestsetGenerator.from_langchain(llm, llm, embeddings)
 
 # Generate test dataset
 testset_size = 1000
